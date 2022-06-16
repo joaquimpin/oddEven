f4e5bb348653e60177abb9ea8440a8ea22aea425
Joaquim Pinda <joaquimpineda@gmail.com>
2022-06-16 21:23:40 +0200

    Initial Commit

---
+++ oddEven.java
@@ -0,0 +1,19 @@
+public class oddEven {
+    public static void main (String[] args){
+        oddEven();
+    }
+    public static void oddEven (){
+        int odd = 0;
+        int even = 0;
+        for (int i = 1; i <= 100; i++) {
+            int num = (int) Math.round(Math.random()*100);
+            if (num%2 == 0 ) {
+                odd ++;
+            } else {
+                even ++;
+            }
+        }
+        System.out.println("odd "+ odd + ","+ even +" even");
+
+    }
+}
